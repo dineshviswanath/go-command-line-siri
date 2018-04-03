a77fa56acb1dc61c1dbf7d1db3f2336f72da34f8
Dinesh V <dinesh.viswanath@nttdata.com>
2018-04-03 22:58:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Personal Command-line Assistant
+```
+export ENV_TWT_CONSUMER_KEY="3yBdRC..."
+138 export ENV_TWT_CONSUMER_SECRET="1AButlZjbaD..."
+139 export ENV_TWT_TOKEN_KEY="18602..."
+140 export ENV_TWT_TOKEN_SECRET="1mau..."
+
+go run siri.go "this is my first tweet"
+
+$ go build siri.go
+$ ls
+siri siri.go
+
+ ./siri "this is my first tweet"
+```
+
+
+## ToDo
++ Send Tweets
+- Clean trash bin
+- Clean docker images
+- Clean desktop
+- Hide Notifications
+- Start DND
---
+++ siri.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+func main() {
+	consumerKey := os.Getenv("ENV_TWT_CONSUMER_KEY")
+	consumerSecret := os.Getenv("ENV_TWT_CONSUMER_SECRET")
+	tokenKey := os.Getenv("ENV_TWT_TOKEN_KEY")
+	tokenSecret := os.Getenv("ENV_TWT_TOKEN_SECRET")
+
+	msg := os.Args[1]
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(tokenKey, tokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Send a Tweet
+	tweet, resp, err := client.Statuses.Update(msg, nil)
+
+	if err != nil {
+		fmt.Println(`Error`)
+		fmt.Print(err)
+	}
+
+	fmt.Printf("> Tweet sent", tweet, resp)
+
+}
