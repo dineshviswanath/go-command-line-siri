4a3a7feece3b55c5ae7d81f58bd3e69ef3e6286e
Dinesh V <dinesh.viswanath@nttdata.com>
2018-04-08 23:08:12 +0100

    Update function structure and code cleanup

--- siri.go
+++ siri.go
@@ -9,12 +9,30 @@ import (
 )
 
 func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println(`Usuage: siri <message_to_tweet>`)
+		return
+	}
+
+	msg := os.Args[1]
+
+	if ok := tweet(msg); ok {
+		fmt.Println("Tweet sent :)")
+	} else {
+		fmt.Println("Tweet could not sent :(")
+	}
+}
+
+func tweet(msg string) bool {
+
+	isTweetSent := false
+
 	consumerKey := os.Getenv("ENV_TWT_CONSUMER_KEY")
 	consumerSecret := os.Getenv("ENV_TWT_CONSUMER_SECRET")
 	tokenKey := os.Getenv("ENV_TWT_TOKEN_KEY")
 	tokenSecret := os.Getenv("ENV_TWT_TOKEN_SECRET")
 
-	msg := os.Args[1]
 	config := oauth1.NewConfig(consumerKey, consumerSecret)
 	token := oauth1.NewToken(tokenKey, tokenSecret)
 	httpClient := config.Client(oauth1.NoContext, token)
@@ -28,8 +46,9 @@ func main() {
 	if err != nil {
 		fmt.Println(`Error`)
 		fmt.Print(err)
+	} else {
+		isTweetSent = true
 	}
 
-	fmt.Printf("Tweet sent!")
-
+	return isTweetSent
 }
